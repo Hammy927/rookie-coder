2279cd8ea65021e9cf72c24ed52a14c85d3d9455
hammy <18500188498@163.com>
2021-05-17 16:06:36 +0800

    <feature>init

---
+++ src/test/java/hammy/rookie/RookieApplicationTests.java
@@ -0,0 +1,13 @@
+package hammy.rookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RookieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
